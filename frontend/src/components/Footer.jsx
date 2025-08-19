6e43dfccc0557f9b288e71f401679242f968b96e
DanielLR16 <daniellr_16@hotmail.com>
2025-08-19 11:13:16 +0200

    Creo el componente Footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer className="w-full bg-white border-t border-gray-300 py-2 flex justify-center">
+      <p
+        className="font-['Roboto'] font-normal text-[12px] leading-[100%] tracking-[0.17px]"
+        style={{ color: 'var(--color-text-disabled, #9A9A9A)' }}
+      >
+        Prueba técnica Minderest 2025
+      </p>
+    </footer>
+  );
+}
