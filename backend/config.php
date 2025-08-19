dc94587d3888e31958fba783a0648d6e7957df56
DanielLR16 <daniellr_16@hotmail.com>
2025-08-19 11:56:20 +0200

    Añado los CORS

--- backend/config.php
+++ backend/config.php
@@ -1,4 +1,17 @@
 <?php
+// ----------------------
+// CORS
+// ----------------------
+header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+// Manejar preflight request (OPTIONS)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
 $host = 'localhost';
 $db   = 'product_matching';  
 $user = ''; // user BD /** CHANGE **/ 
